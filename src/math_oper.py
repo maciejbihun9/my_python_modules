95a5fc25888cae7e629e78314a0b6189a61691cc
MBihun <mac.bih@wp.pl>
2017-07-20 11:26:08 +0200

    data_manager description update

--- src/math_oper.py
+++ src/math_oper.py
@@ -5,5 +5,12 @@ from numpy import *
 class MathOper:
 
     @staticmethod
-    def values_between(data_array: ndarray, min: float, max: float):
+    def values_between(data_array: ndarray, min: float, max: float) -> ndarray:
+        """
+        Get array with values that indicates weather are between min and max
+        :param data_array: data array to check(Size does not metter)
+        :param min: Minimal value
+        :param max: Maximal value
+        :return: True/False ndarray
+        """
         return logical_and(data_array >= min, data_array <= max)
